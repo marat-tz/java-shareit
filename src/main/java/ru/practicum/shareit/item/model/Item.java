2678b2445110ef13c3743fae669ca482c9a3bd16
Marat Tazhetdinov <unlim089@yandex.ru>
2025-02-03 22:01:16 +0300

    Add Item model

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Value;
+import ru.practicum.shareit.user.User;
+
+@Value
+@Builder(toBuilder = true)
 public class Item {
+    @NotNull
+    User owner;
+
+    @NotBlank
+    String name;
+
+    String description;
+
+    boolean available;
 }
