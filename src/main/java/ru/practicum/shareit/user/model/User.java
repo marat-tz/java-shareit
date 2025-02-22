fda65051ae496a6c5ea6ba36247a3962de2be272
Marat Tazhetdinov <unlim089@yandex.ru>
2025-02-22 11:30:03 +0300

    Add @Column on User fields

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,19 +1,17 @@
 package ru.practicum.shareit.user.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.Value;
 
 @Entity
-@Table(name = "users", schema = "public")
-@Getter
+@Table(name = "users")
+@Value
 @Builder(toBuilder = true)
 public class User {
 
@@ -21,10 +19,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @NotBlank(message = "Электронная почта не может быть пустой")
-    @Email(message = "Электронная почта должна соответствовать шаблону name@domain.xx")
+    @Column(unique = true, nullable = false)
     String email;
 
-    @NotBlank(message = "Имя не может быть пустым")
+    @Column(nullable = false)
     String name;
 }
