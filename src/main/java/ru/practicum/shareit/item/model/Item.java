4ddd0d62570bcb39c723d2582966dca02b9787ac
Marat Tazhetdinov <unlim089@yandex.ru>
2025-02-22 11:35:03 +0300

    Add @Column on Item fields

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,19 +1,23 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Value;
 import ru.practicum.shareit.user.model.User;
 
 @Entity
-@Table(name = "items", schema = "public")
-@Getter
+@Table(name = "items")
+@Value
 @Builder(toBuilder = true)
 public class Item {
 
@@ -21,15 +25,16 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
     User owner;
 
-    @NotBlank
+    @Column(nullable = false)
     String name;
 
-    @NotBlank
+    @Column(length = 512)
     String description;
 
-    @NotNull
+    @Column(nullable = false)
     Boolean available;
 }
